830aaa5d55973d0c1f98e64f44bb23f88d7fbd96
fdel-car <fdel-car@student.42.fr>
2019-06-09 17:20:03 +0200

    Almost done, just missing secondary program

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('data.csv')
+assert(len(data.columns) == 2)
 X = data.iloc[:, 0]
 
 # Data normalization in order to avoid potential overflow
@@ -17,26 +18,40 @@ Y_Max = np.max(Y)
 
 m = 0
 c = 0
-L = 0.02
-n = len(X)
+L = 0.1
+n = float(len(X))
 assert(len(Y) == n)
 
-it = 10000
+it = 2200
 for _ in range(it):
-    # while True:
     Y_Pred = m * X_Norm + c
-    D_M = (-2.0 / n) * sum(X_Norm * (Y - Y_Pred))
-    D_C = (-2.0 / n) * sum(Y - Y_Pred)
-    m = m - L * D_M
-    c = c - L * D_C
-    # print D_M
-    # if (abs(D_M) < 0.04):
-    # break
+    m = m - L * (sum((Y_Pred - Y) * X_Norm) / n)
+    c = c - L * (sum(Y_Pred - Y) / n)
 
 m /= limit
-print (m, c)
+# print sum((Y - (m * X + c)) ** 2) / n
+
+
+def displayPrecision():
+    Sig_X = sum(X)
+    Sig_Y = sum(Y)
+    Sig_XX = sum(_ * _ for _ in X)
+    Sig_XY = sum(_ * Y[i] for i, _ in enumerate(X))
+    __c = (Sig_Y * Sig_XX - Sig_X * Sig_XY) / ((n * Sig_XX) - Sig_X ** 2)
+    __m = (n * Sig_XY - Sig_X * Sig_Y) / (n * Sig_XX - Sig_X ** 2)
+    D_M = abs((m - __m) / __m * 100)
+    D_C = abs((c - __c) / __c * 100)
+    print "The algorithm precision (similarity with the result given by the close end formula for a simple linear regression) is {:.2f}%.".format(
+        max(100 - (D_M + D_C) / 2, 0))
+
+
+displayPrecision()
+# Write fo file
+
 plt.title("Linear regression")
+plt.xlabel(data.columns[0].capitalize())
+plt.ylabel(data.columns[1].capitalize())
 plt.scatter(X, Y, color="red")
 Z = np.array([X_Min, X_Max])
-plt.plot(Z, c + Z * m)
+plt.plot(Z, m * Z + c)
 plt.show()
